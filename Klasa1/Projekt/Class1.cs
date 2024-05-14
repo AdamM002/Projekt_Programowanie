88d27ae4a0b83f0f7743c2e1ab3e70695b11b70e
LochNessy <s210899@sggw.edu.pl>
2024-05-14 20:36:28 +0200

    testowe

--- Klasa1/Projekt/Class1.cs
+++ Klasa1/Projekt/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,6 @@ namespace Projekt
     public class Class1
     {
         public int licznik;
+        public string test;
     }
 }
