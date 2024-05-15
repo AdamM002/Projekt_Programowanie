ad467617874b9b3d7661b3b38a96f3dda17f2cf8
ngojen13 <naimad1337@gmail.com>
2024-05-15 22:29:36 +0200

    Update Przedmiot.cs

--- Klasa1/Projekt/Przedmiot.cs
+++ Klasa1/Projekt/Przedmiot.cs
@@ -11,7 +11,15 @@ namespace Projekt
         protected string nazwa;
         protected string rzadkosc;
         protected string kolekcja;
+        protected string img;
 
+        public Przedmiot (string nazwa, string rzadkosc, string kolekcja, string img) 
+        {
+            this.nazwa = nazwa;
+            this.rzadkosc = rzadkosc;
+            this.kolekcja = kolekcja;
+            this.img = img;
+        }
         
     }
 }
