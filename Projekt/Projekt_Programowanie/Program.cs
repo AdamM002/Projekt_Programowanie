cb22cf63603858414082dc29c891fb73fa0cfaeb
AdamMokrzycki <92626198+AdamM002@users.noreply.github.com>
2024-05-18 12:20:23 +0200

    Poprawiłem ułożenie plików

---
+++ Projekt/Projekt_Programowanie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_Programowanie
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var myForm = new Form1();
+            myForm.Show();
+            Application.Run();
+        }
+    }
+}
---
+++ Projekt/Przedmiot/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class Przedmiot
+    {
+        protected string nazwa;
+        protected string rzadkosc;
+        protected string kolekcja;
+        protected string img;
+
+        public Przedmiot(string nazwa, string rzadkosc, string kolekcja, string img)
+        {
+            this.nazwa = nazwa;
+            this.rzadkosc = rzadkosc;
+            this.kolekcja = kolekcja;
+            this.img = img;
+        }
+
+    }
+}
